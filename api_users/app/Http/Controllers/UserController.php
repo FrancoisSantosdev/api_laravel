4266732d9eeb43348e3214605d4591c3f7ed35da
Francois dos Santos <francoissantos.dev@outlook.com>
2024-02-20 02:50:46 -0300

    Atualizado com metodo de validação de erros

--- api_users/app/Http/Controllers/UserController.php
+++ api_users/app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 
 class UserController extends Controller
@@ -21,10 +22,20 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+        'name' => 'required',
+        'email' => 'required|email',
+        'proficao' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+        return response()->json(['errors' => $validator->errors()], 422);
+        }
+
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->profession = $request->profession;
+        $user->proficao = $request->proficao;
         $user->save();
 
         return response()->json(['message' => 'User created successfully'], 201);
@@ -48,4 +59,6 @@ class UserController extends Controller
 
         return response()->json(['message' => 'User deleted successfully'], 200);
     }
+
+
 }
