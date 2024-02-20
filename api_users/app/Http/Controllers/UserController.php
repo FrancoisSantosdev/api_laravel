287303977f8af4c2fa749fbf035b816b7f29bc71
Francois dos Santos <francoissantos.dev@outlook.com>
2024-02-20 01:34:44 -0300

    atualizações de sintaxe para column profession

--- api_users/app/Http/Controllers/UserController.php
+++ api_users/app/Http/Controllers/UserController.php
@@ -24,7 +24,7 @@ class UserController extends Controller
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->proficao = $request->profession;
+        $user->profession = $request->profession;
         $user->save();
 
         return response()->json(['message' => 'User created successfully'], 201);
@@ -35,7 +35,7 @@ class UserController extends Controller
         $user = User::findOrFail($id);
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->proficao = $request->profession;
+        $user->profession = $request->profession;
         $user->save();
 
         return response()->json(['message' => 'User updated successfully'], 200);
